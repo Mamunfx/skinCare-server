dc1c5d4dc4e7b797ef7af774860cb9b68c61e7ee
Mamunfx <md01993049420@gmail.com>
2025-01-02 01:56:16 +0600

    done

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5175'],
   credentials: true
 }));
 app.use(express.json());
